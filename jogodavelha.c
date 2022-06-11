2bbb2f5b564bdc0a93890d2ebe5134cfdc12aa2e
0x6775737461 <fabiogustavo@tuta.io>
2022-06-11 14:10:41 -0400

    função pro banner de instruções de uso

--- jogodavelha.c
+++ jogodavelha.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int verificarVelha(char tab[3][3], char v);
+void bannerInicial(void);
+
+int main(void) {
+  char tabuleiro[3][3];
+  int l, c;
+
+  // msg com instruções de uso
+  bannerInicial();
+
+  for (l = 0; l < 3; l++) {
+    for (c = 0; c < 3; c++) {
+
+      printf("Digite o valor da posicao [%d, %d]: ", l + 1, c + 1);
+      fflush(stdin);
+      scanf("%c", &tabuleiro[l][c]);
+    }
+  }
+  if (verificarVelha(tabuleiro, 'x'))
+    printf("\n\nParabens o jogador que usou 'x' venceu");
+  else {
+    if (verificarVelha(tabuleiro, 'o'))
+      printf("\n\nParabens o jogador que usou 'o' venceu");
+    else {
+      printf("\n\nDeu velha");
+    }
+  }
+  return 0;
+}
+
 int verificarVelha(char tab[3][3], char v) {
   if ((tab[0][0] == v) && (tab[0][1] == v) && (tab[0][2] == v))
     return 1;
@@ -24,31 +54,12 @@ int verificarVelha(char tab[3][3], char v) {
   return 0;
 }
 
-int main(void) {
-  char tabuleiro[3][3];
-  int l, c;
-  printf("Jogo da velha");
-  printf("\n\nUse X ou O para jogar");
-  printf("\n\nRegrinhas do jogo: ");
-  printf("\n1- Cada jogador tem uma vez de jogar! ");
-  printf("\n2- Ganha primeiro quem fazer uma linha ou diagonal com a sua forma "
-         "escolhida sendo X ou O");
-  for (l = 0; l < 3; l++) {
-    for (c = 0; c < 3; c++) {
-
-      printf("Digite o valor da posicao [%d, %d]: ", l + 1, c + 1);
-      fflush(stdin);
-      scanf("%c", &tabuleiro[l][c]);
-    }
-  }
-  if (verificarVelha(tabuleiro, 'x'))
-    printf("\n\nParabens o jogador que usou 'x' venceu");
-  else {
-    if (verificarVelha(tabuleiro, 'o'))
-      printf("\n\nParabens o jogador que usou 'o' venceu");
-    else {
-      printf("\n\nDeu velha");
-    }
-  }
-  return 0;
+void bannerInicial(void) {
+  puts(""
+       "Jogo da velha\
+  \n\nUse X ou O para jogar;\
+  \n\nRegrinhas do jogo:\
+  \n1- Cada jogador tem uma vez de jogar!\
+  \n2- Ganha primeiro quem fazer uma linha \
+  \nou diagonal com a sua forma escolhida sendo X ou O\n");
 }
